de9d25f61b9c70ced2fcc5c0504840dc270ac9a1
Juan Patrón <josejuanpatron1630@gmail.com>
2022-06-29 00:41:41 -0500

    Vlidacion de numeros negativos

--- js/recurive.js
+++ js/recurive.js
@@ -4,15 +4,20 @@ var escalones = 50;
 const bajaEscalera = (escalon) => {
     console.log('Escalera con', escalon);
     // caso base - sabemos que es el ulitmo y que termina
+   
+   if(escalon < 0){
     if( escalon === 0 ){
         console.log('Se termino la escalera');
         return
     }else{
-        escalon = escalon - 1;
-        console.log(escalon);
-        escalones = escalon;
-        bajaEscalera( escalones );
+        setTimeout(() => {
+            escalon = escalon - 1;
+            console.log(escalon);
+            escalones = escalon;
+            bajaEscalera( escalones );
+        }, 1000);
     }
+   }
 }
 
 // factorial de un numero, con recursividad
